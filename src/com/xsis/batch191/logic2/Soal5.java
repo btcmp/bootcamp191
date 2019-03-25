bc37ee48d80a520399a77d4247b3637d78188561
arrizaqu@yahoo.com <arrizaqu@yahoo.com>
2019-03-25 17:30:21 +0700

    add sample logic day 2

--- src/com/xsis/batch191/logic2/Soal5.java
+++ src/com/xsis/batch191/logic2/Soal5.java
@@ -12,7 +12,7 @@ public class Soal5 {
 		//System.out.println(split[1]);
 		String[] hasil = new String[split.length];
 		for(int wr = 0; wr < split.length; wr++ ) {
-			String data = "";
+			String data = ""; 
 			for(int i = 0; i < split[wr].length(); i++) {
 				if(i == 0 || i == split[wr].length()-1) {
 					data += split[wr].charAt(i);
